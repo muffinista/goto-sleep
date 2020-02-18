654d29af7555b9cc61450067f487ba657495f0dd
Colin Mitchell <colin@muffinlabs.com>
2020-02-18 12:44:12 -0500

    Add some code for locking the screen

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var GotoSleep;
+let GotoSleep;
 const useBindings = (process.platform === 'darwin' || process.platform === 'win32');
 
 if ( useBindings ) {
@@ -8,29 +8,31 @@ else {
   const which = require('which');
   const exec = require('child_process').exec; 
   const runCommand = function(str) {
-    // console.log(str);
     return exec(str).stderr.pipe(process.stderr);
-  }
-  GotoSleep = {
-    gotoSleep: function() {
-      if ( which.sync("xdg-screensaver", { nothrow: true }) ) {
-        runCommand('xdg-screensaver lock');
+  };
+  const handler = function() {
+    if ( which.sync("xdg-screensaver", { nothrow: true }) ) {
+      runCommand("xdg-screensaver lock");
+    }
+    else if ( which.sync("gnome-screensaver-command", { nothrow: true }) ) {
+      runCommand("gnome-screensaver-command -l");
+    }
+    else {
+      //
+      // try some random things
+      //
+      if ( which.sync("dbus-send", { nothrow: true }) ) {
+        runCommand("dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock");
       }
-      else if ( which.sync("gnome-screensaver-command", { nothrow: true }) ) {
-        runCommand('gnome-screensaver-command -l');
-      }
-      else {
-        //
-        // try some random things
-        //
-        if ( which.sync("dbus-send", { nothrow: true }) ) {
-          runCommand('dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock');
-        }
-        if ( which.sync("xset", { nothrow: true }) ) {
-          runCommand('xset dpms force suspend');
-        }
+      if ( which.sync("xset", { nothrow: true }) ) {
+        runCommand("xset dpms force suspend");
       }
     }
+  };
+
+  GotoSleep = {
+    gotoSleep: handler,
+    lockScreen: handler
   };
 }
 module.exports = GotoSleep;
